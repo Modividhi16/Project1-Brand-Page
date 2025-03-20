4a6ae0f05214e6c652290c213a63d32e5fc9cdbf
Vidhi Modi <modividhi1695@gmail.com>
2025-03-20 12:12:10 -0500

    Create basic brand page with React

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navigation from "./components/Navigation";
+
 function App() {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="logo"/>
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>
-          Login
-        </button>
-      </nav>
+      <Navigation />
+      <HeroSection />
     </div>
   );
 }
